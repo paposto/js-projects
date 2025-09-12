1dc95670a7c5a3867f22a655fac4772b8f5d5a3b
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-12 16:20:19 +0200

    string-example

---
+++ string.js
@@ -0,0 +1,34 @@
+let str = 'Coding Factory'; 
+
+// parse
+let s1 = str.substring(1, 5);   //odin
+let s2 = str.slice(1, 5);   //odin
+let s3 = str.slice(-5);     //ctory
+
+//split
+let tokens = str.split(" ") //['Codng', 'Factory']
+console.log(tokens); 
+
+//strings are char arrays
+console.log(str.charAt(0));     //0
+let index = str.indexOf('o');   //1
+let index2 = str.lastIndexOf('o');  //11 
+
+//equality
+let str1 = "Athens"; 
+let str2 = "Athens";
+if (str1 == str2) { 
+    console.log("Equal")
+} else { console.log("Not equal") }; 
+
+//case insensitive
+if (str1.toUpperCase() == str2.toUpperCase()) { //normalization
+    console.log("Equal")
+} else { console.log("Not equal") }; 
+
+//trim
+let aString = "   Coding"; 
+let trimmed = aString.trim(); 
+
+//concat με + ή/και .concat()
+let concatenated = str1 + str2 + aString;  
