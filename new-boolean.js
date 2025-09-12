d5e191547082a74a9786a2b4af17e05571c73298
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-12 15:27:17 +0200

    First commit

---
+++ new-boolean.js
@@ -0,0 +1,9 @@
+let inputName = undefined; 
+let username = inputName || 'default user'; 
+
+console.log(username);  //default user
+
+let value = 0; 
+let result = value || 'default value'; 
+
+console.log(result); //default value
---
+++ number.js
@@ -0,0 +1,12 @@
+if (number.isInteger(42)) {
+    console.log('42 is an integer'); 
+}   else {
+    console.log('42 is not an integer'); 
+}
+
+console.log(Number.MIN_VALUE); 
+console.log(Number.MAX_VALUE); 
+console.log(Number.POSITIVE_INFINITY); 
+console.log(Number.NEGATIVE_INFINITY); 
+
+let aNum = 10
