2bd33ed1535c9d9ed23f4b8b1cef93831dcbdbf0
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-14 14:46:44 +0200

    fix: add more comments

--- arrays.js
+++ arrays.js
@@ -8,4 +8,24 @@ const arr = [...arr1, arr2]     //spread operator gives a shallow copy
 console.log(arr2.length);   //5
 console.log(arr2[0]);       // 1
 console.log(arr2[arr2.length - 1]); //4
+
+// Join an array with a delimiter and returns a string
 console.log(arr2.join(','))     // γίνεται string 
+
+// Traverse
+//Classic for
+for (let i = 0; i < arr2.length; i++) {
+    console.log(arr2[i])
+}
+
+// Enhanced for with for.. of
+for (let item of arr2) {
+    console.log(item)
+}
+
+
+//Functional way, not iterative (είναι μέθοδος, μέσα υποκρύπτεται η for)
+arr2.forEach((item, index) => {
+    console.log(`Value: ${item}, index: ${index}`);
+});
+
