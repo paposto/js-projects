448c1b21724a90e026f08456508e09aa15bfccb6
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-15 18:21:27 +0200

    array-iterative-methods-exercises

---
+++ arrays-iterative-methods.js
@@ -0,0 +1,39 @@
+const students = ['Νίκη', 'Ανδρέας', 'Γιάννη']
+
+students.forEach(function(st) {
+    console.log(stu)
+})
+
+students.forEach(function(stu, index, arr) {
+    console.log(index, stu, arr)
+})
+
+students.forEach(function(val, index) {
+    console.log(index, val)
+})
+
+students.forEach(function(stu, _, arr) {
+    console.log(stu, arr)
+})
+
+//Filter 
+
+let filtered = students.filter(stu => stu === 'Γιάννης')
+
+const users = [
+    {firstname: "Ανδρέας", age: 25, active: true, email: 'andrew@gmail.com'}, 
+    {firstname: "Νίκη", age: 20, active: false, email: 'niki@gmail.com'}
+]
+
+const activeUsers = users.filter(u => u.active)
+
+const emails = users.map(user => user.email)        // ['andrew@gmail.com', niki@gmail.com]
+
+const numbers = [1, 2, 3, 4, 5]
+let sum = numbers.reduce((acc, val) => total + val, 0)
+let avg = numbers.reduce((acc, val) => total + val, 0) / numbers.length
+
+
+
+
+
