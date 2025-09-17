a8a767cbfe14dcb107c4dcfb4e357d89264af981
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-17 11:17:53 +0200

    array-sort-exercises

--- arrays-iterative-methods.js
+++ arrays-iterative-methods.js
@@ -33,6 +33,20 @@ const numbers = [1, 2, 3, 4, 5]
 let sum = numbers.reduce((acc, val) => total + val, 0)
 let avg = numbers.reduce((acc, val) => total + val, 0) / numbers.length
 
+//Find and FindIndex
+//Find returns a value, not an array
+const user = users.find(user => user.email === 'andrew@gmail.com')
+
+const userIndex = users.findIndex(user => user.email === 'andrew@gmail.com')
+
+//Some and every
+const isOver24 = users.some(user => user.age >= 24)     //true, έστω ένας πάνω ή ίσο από 24
+const isAllOver24 = users.every(user => user.age >= 18) //true, γιατί όλοι είναι πάνω από 18
+
+//Flatmap
+const result = users.flatMap(user => [user, user.age / 2])  // [{12.5, {}, 10}]
+
+
 
 
 
