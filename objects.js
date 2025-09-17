daeb7807a80a8b405747f7eaddda263b354d7b8e
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-17 15:38:19 +0200

    feat: clear

--- objects.js
+++ objects.js
@@ -1,7 +1,14 @@
 const point3D = new Object()
-point3d.x = 5
-point3d.y = 10
-point3d.z = 20 
+point3D.x = 5
+point3D.y = 10
+point3D.z = 20 
+
+const user = {
+    firstname: 'Alice', 
+    lastname: 'Wonderland', 
+    hobbies: [{id:1, title: "Hobbie1"}, {id: 2, title: "Tennis"}]
+
+}
 
 const point2D = {
     x: 10,
@@ -12,7 +19,7 @@ const point2D = {
 }
 
 console.log(point2D.x)
-console.log(point2['x'])
+console.log(point2D['x'])
 
 point2D.showX = function() {
     return `${this.x}` 
@@ -22,7 +29,7 @@ for (let key in point3D) {
     console.log(`${key}: ${point3D[key]}`)
 }
 
-for (const[key, value] of Object.entries(point3D)) {
+for (const[key, value] of Object. entries(point3D)) {
     console.log(`${key}: ${value}`)
 }
 
@@ -39,3 +46,31 @@ const cart = {
 cart.addItem('Milk', 2)
 cart.addItem('Eggs', 5)
 console.log(cart.calculateTotal())  // 7
+
+// Object destructuring
+const {x, y, z} = point3D
+console.log(`${x}, ${y}, ${z}`)
+
+
+// Without destructing - verbose 
+function display3DPoint(point) {
+    console.log(`x: ${point['x']}`)
+    console.log(`x: ${point['y']}`)
+    console.log(`x: ${point['z']}`)
+
+}
+
+// With destructing - not verbose 
+function display3DPoint(x, y, z = 'Not provided') {
+    console.log(`x: ${x}`)
+    console.log(`x: ${y}`)
+    console.log(`x: ${z}`)
+}
+
+const p2 = {
+    x: 15, 
+    y: 18, 
+    z: 22
+}
+
+display3DPoint2(p2)
