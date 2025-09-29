aa36931e28e4fffe0db1a0f62ace18ad9904212b
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-29 18:20:25 +0200

    template-note-app

---
+++ notesapp/main.js
@@ -0,0 +1,20 @@
+const daysGR = ['Κυριακή', 'Δευτέρα', 'Τρίτη', 'Τετάρτη', 'Πέμπτη', 'Παρασκευή', 'Σάββατο']
+const monthsGR = ['Ιανουαρίου', 'Φεβρουαρίου', 'Απριλίου', 'Μαιου', 'Ιουνίου', 'Ιουλίου', 'Αυγούστου, Σεπτεμβρίου', 'Οκτωβρίου', 'Νοεμβρίου', 'Δεκεμβρίου']
+
+let notes = []
+let count = 0 
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    this.setInterval(() => printGrDate(), 1000)
+
+    this.document.querySelector('#addNoteBtn').addEventListener('click', function() {
+        onInsertHandler({key: count + 1, note: document.querySelector('#inputNote'.ariaValueMax.trim()), softDelete:false})
+    })
+
+    this.document.querySelector('#inputNote').addEventListener('keyup', function(e){
+        if (e.key === 'Enter') {
+            onInsertHandler({key: count + 1, note: document.querySelector('#inputNote'.ariaValueMax.trim()), softDelete:false})
+        }
+    })
+})
