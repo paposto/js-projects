6a755ee135e309d5d672f40e122d535cfc9dc909
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-09-30 18:00:41 +0200

    feat: add model and view methods

--- notesapp/main.js
+++ notesapp/main.js
@@ -33,5 +33,54 @@ window.addEventListener('DOMContentLoaded', function() {
 
         const dateStr = `${dayStr}, ${date} ${monthStr} ${year}`
         const timeStr = `${(hours < 10) ? '0' : ''}${hours}:${(minutes < 10) ? '0' : ''}${minutes}${(seconds < 10) ? '0' : ''}${seconds}`
+
+        document.querySelector('#dateTxt').innerHTML = `${dateStr}<br>${timeStr}`
+    }
+
+    // Controller
+    function onInsertHandler(obj) {
+        if (!obj?.note) return 
+
+        insertNote(obj)
+        reset()
+    }
+
+    // Model
+    function insertNote(obj) {
+        notes = [...notes, obj]
+        count++
+        
+        renderNotes()
+    }
+    
+    // View
+    function renderNotes() {
+        const container = document.querySelector('#notesWrapper')
+
+        container.innerHTML = notes.map(note => `<div id="${'noteTemplate' + note.key}" class="flex justify-between item-center px-[2px] border-b border=black">
+            
+                <div>
+                    <input type="checkbox" id="${'noteCheck' + note.key}" onclick="strikeThrough(${note.ke})" class="w-[25px] h-[25px] mr-[5px]" ${note.softDeleted ? 'checked': ''}>
+                    <label id="${'noteTxt + note.key'}" for="${'noteCheck' + note.key}" class="w-[200px] max-h-[150px] overflow-hidden break-words whitespace-normal text-base ${note.softDeleted ? 'line-through text-gray-500' : ''}">${note.note}</label>
+            </div>
+            <button type="button" id="${'noteDelBtn' + note.key}" class="w-[35px] h-[35px] border border-black rounded-full" onclick=:deketeNote(${note.key})">X</button>
+             
+        </div>`).join("")
+    }
+
+    // Model
+    function strikeThrough(key) {
+        notes = notes.map(note => note.key === key ? {...note, softDeleted: !note.softDeleted} : {...note}) 
+        renderNotes()
+    }
+
+    function deleteNote(key) {
+        notes = notes.filter(note => note.key !== key)
+        renderNotes()
+    }
+
+    // View 
+    function reset() {
+        document.querySelector('#inputNote').value = ''
     }
 })
