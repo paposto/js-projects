0c502363aefbac5bed850597c8f68490055f64d4
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-10-01 15:38:13 +0200

    feat: add model and view methods

--- counterapp/main.js
+++ counterapp/main.js
@@ -2,13 +2,53 @@ const DEFAULT = 0
 let counter = DEFAULT   // State - Data
 
 window.addEventListener('DOMContentLoaded', function() {
-    
+
     this.window.document.querySelector("#btnDecr").addEventListener('click', () => onDecreaseClicked())
     this.window.document.querySelector("#btnReset").addEventListener('click', () => onResetClicked())
-    this.window.document.querySelector("#btnInc").addEventListener('click', () => onIncreaseClicked())
+    this.window.document.querySelector("#btnIncr").addEventListener('click', () => onIncreaseClicked())
 
 })
 
 // Controllers
+function onDecreaseClicked() {
+    decreaseCounter()
+}
 
+function onResetClicked() {
+    resetCounter()
+}
 
+function onIncreaseClicked() {
+    increaseCounter()
+}
+
+// Model
+function decreaseCounter() {
+    counter--; 
+    render()
+}
+
+function resetCounter() {
+    counter = DEFAULT
+    render()
+}
+
+function increaseCounter() {
+    counter++
+    render()
+}
+
+// View
+
+function render() {
+    const counterDOM = document.guerySelector('#counter')
+    counterDOM.textContent = counter
+    styleCounter(counterDOM)
+}
+
+function styleCounter(counterDOM) {
+    counterDOM.classlist.toggle('color-green', counter > 0)
+    counterDOM.classlist.toggle('color-red', counter < 0)
+    counterDOM.classlist.toggle('color-black', counter === 0)
+
+}
