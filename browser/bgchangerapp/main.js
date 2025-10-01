a8fde93b1890c57d2a4a3aa0c07a30aeb74119f9
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-10-01 17:48:06 +0200

    feat: add basic random bg colors logic

---
+++ browser/bgchangerapp/main.js
@@ -0,0 +1,29 @@
+let color = undefined
+
+window.addEventListener("DOMContentLoaded", function() {
+    this.document.querySelector('#btn').addEventListener('click', function() {
+        onClickedMeClicked()
+    })
+})
+
+//  Controllers
+function onClickedMeClicked() {
+    updateBg()
+}
+
+// Model
+function updateBg() {
+    color = getBgColor()    // color = 'green'
+    renderBg()
+}
+
+function getBgColor() {
+    const colors = ['black', 'red', 'green', 'blue', 'yellow']
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function renderBg() {
+    document.getElementById('color').innerHTML = color 
+    document.body.style.backgroundColor = color
+}
+
